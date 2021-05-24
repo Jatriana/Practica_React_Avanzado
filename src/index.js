096e6aa756cb34b6fe4632f5f2c5fccdf1b93863
jaime Andres Triana <zubov2000@gmail.com>
2021-05-24 22:47:03 +0200

    se implemnte en index,  Store despues de storage.get(se lee si el usuario esta logeado)  se  pasa el accestoken

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import { configureClient } from './api/client';
 import storage from './utils/storage';
 import './index.css';
 import App from './components/app';
+import configureStore from './store';
 
 const accessToken = storage.get('auth');
 configureClient({ accessToken });
 
+const store = configureStore({ preloadedState: { auth: !!accessToken } });
+console.log(store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <Router>
--- src/store/index.js
+++ src/store/index.js
@@ -6,4 +6,4 @@ const configureStore = ({ preloadedState }) => {
 
   return store;
 };
-export default configureStore
+export default configureStore;
