0eeb9496e1ceab6048efa9d0523b9e7e7a159eae
jaime Andres Triana <zubov2000@gmail.com>
2021-05-28 17:56:35 +0200

    en el root hay que pase el history al router

--- src/root.js
+++ src/root.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import App from './components/app';
 
-export default function Root({ store }) {
+export default function Root({ store, history }) {
   return (
     <Provider store={store}>
-      <Router>
+      <Router history={history}>
         <App />
       </Router>
     </Provider>
